e3e90f074465fc360983fbc900188d9adadca6ec
Katarzyna Maculewicz <qla.katarzyna@gmail.com>
2023-03-10 13:11:28 +0000

    js functions added

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
-console.log("Hello world");
+{
+      const welcome = () => {
+            console.log("Hello world");
+      }
 
-let button = document.querySelector(".header__button")
-let body = document.querySelector(".body");
+      const toggleBackground = () => {
+            const body = document.querySelector(".body");
+            const button = document.querySelector(".header__button")
+            
+            body.classList.toggle("body--dark");
+            button.interText === (button.interText === "dark mode") ? "light mode" : "dark mode";
+      };
 
-button.addEventListener("click", () => {
-      body.classList.toggle("body--dark");
-      button.interText === (button.interText === "dark mode") ? "light mode" : "dark mode";
-});
+      const init = () => {
+            const button = document.querySelector(".header__button")
+
+            button.addEventListener("click", toggleBackground);
+
+            welcome();
+      };
+
+      init();
+}
